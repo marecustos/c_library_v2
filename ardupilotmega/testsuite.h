a371ca8a08139f8e95dc54c206bc594cdefc4b0b
MblRobotics <mahmoudbenltaifa@gmail.com>
2024-03-06 13:53:42 +0100

    add new payload control message

--- ardupilotmega/testsuite.h
+++ ardupilotmega/testsuite.h
@@ -164,6 +164,66 @@ static void mavlink_test_payload_status(uint8_t system_id, uint8_t component_id,
 #endif
 }
 
+static void mavlink_test_custom_payload_control(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_CUSTOM_PAYLOAD_CONTROL >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_custom_payload_control_t packet_in = {
+        17235,"CDEFGHIJKLMNOPQ"
+    };
+    mavlink_custom_payload_control_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.command_value = packet_in.command_value;
+        
+        mav_array_memcpy(packet1.command_target, packet_in.command_target, sizeof(char)*16);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_CUSTOM_PAYLOAD_CONTROL_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_CUSTOM_PAYLOAD_CONTROL_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_custom_payload_control_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_custom_payload_control_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_custom_payload_control_pack(system_id, component_id, &msg , packet1.command_target , packet1.command_value );
+    mavlink_msg_custom_payload_control_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_custom_payload_control_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.command_target , packet1.command_value );
+    mavlink_msg_custom_payload_control_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_custom_payload_control_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_custom_payload_control_send(MAVLINK_COMM_1 , packet1.command_target , packet1.command_value );
+    mavlink_msg_custom_payload_control_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("CUSTOM_PAYLOAD_CONTROL") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_CUSTOM_PAYLOAD_CONTROL) != NULL);
+#endif
+}
+
 static void mavlink_test_sensor_offsets(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -4344,6 +4404,7 @@ static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, 
 {
     mavlink_test_keyboard_command_payload(system_id, component_id, last_msg);
     mavlink_test_payload_status(system_id, component_id, last_msg);
+    mavlink_test_custom_payload_control(system_id, component_id, last_msg);
     mavlink_test_sensor_offsets(system_id, component_id, last_msg);
     mavlink_test_set_mag_offsets(system_id, component_id, last_msg);
     mavlink_test_meminfo(system_id, component_id, last_msg);
